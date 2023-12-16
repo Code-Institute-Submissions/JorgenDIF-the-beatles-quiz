acdc34e84969f53ee70889c68c252564490d7119
JorgenDIF <jorgenjonsson80@gmail.com>
2023-12-16 09:28:39 +0100

    create hide show rules function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,23 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
+
+// Start game function. Opens the dropdown and starts the game with the selected difficulty
+function openDropdown() {
     document.getElementById("diffDropdown").classList.toggle("show");
   }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.start-btn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-btn");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
+
+  function startGame(difficulty) {
+    console.log(`Starting game with difficulty: ${difficulty}`);
+    
+
+    // Hide the dropdown
+    document.getElementById("diffDropdown").classList.remove("show");
   }
- 
- 
+
+ function hideRules() {
+let rules = document.getElementById("rules-container");
+if (rules.style.display === "none") {
+    rules.style.display = "block";
+}
+else {
+    rules.style.display = "none";
+}
+}
