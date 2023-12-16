3fde11eb30bce50c0a9838583a236391dc4fdf72
JorgenDIF <jorgenjonsson80@gmail.com>
2023-12-16 11:30:23 +0100

    add easy questions to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+const questionContainerElement = document.getElementById('question-container')
+
 
 // Start game function. Opens the dropdown and starts the game with the selected difficulty
 function openDropdown() {
@@ -21,3 +23,24 @@ else {
     rules.style.display = "none";
 }
 }
+
+const easyQuestions = [
+{ question : 'Love, Love me ___?', answer: 'do',
+option1: 'do', option2 : 'to', option3: 'more', option4: 'true'},
+
+{ question : 'You say goodbye, and I say ___?', answer: 'hello',
+option1: 'goodbye', option2 : 'hello', option3: 'goodnight', option4: 'goodmorning'},
+
+{ question : 'I get by with a little help from my ___?', answer: 'friends',
+option1: 'friends', option2 : 'family', option3: 'pets', option4: 'neighbors'}
+
+{ question : 'I wanna hold your ___?', answer: 'hand',
+option1: 'hand', option2 : 'heart', option3: 'head', option4: 'hair'},
+
+{ question : 'All you need is ___?', answer: 'love',
+option1: 'love', option2 : 'money', option3: 'quiz', option4: 'friends'},
+
+]
+
+
+    
