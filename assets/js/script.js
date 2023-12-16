8f7763c67ae42df14e6e0a23f236bb193c5e8f23
JorgenDIF <jorgenjonsson80@gmail.com>
2023-12-16 14:29:49 +0100

    update javascript  2 functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,38 @@
 const questionContainerElement = document.getElementById("question-container");
+let shuffleQuestions, currentQuestionIndex;
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+
 
 // Start game function. Opens the dropdown and starts the game with the selected difficulty
 function openDropdown() {
-  document.getElementById("diffDropdown").classList.toggle("show");
+    document.getElementById("diffDropdown").classList.toggle("show");
 }
 
 function startGame(difficulty) {
-  console.log(`Starting game with difficulty: ${difficulty}`);
-
-  // Hide the dropdown
+   
+    console.log(`Starting game with difficulty: ${difficulty}`);
+    
+    // Select the questions based on the difficulty
+    switch (difficulty) {
+      case 'Easy':
+        shuffleQuestions = easyQuestions.sort(() => Math.random() - 0.5);
+        break;
+      case 'Medium':
+        shuffleQuestions = mediumQuestions.sort(() => Math.random() - 0.5);
+        break;
+      case 'Hard':
+        shuffleQuestions = hardQuestions.sort(() => Math.random() - 0.5);
+        break;
+      default:
+        console.error(`Invalid difficulty: ${difficulty}`);
+        return;
+    }
+  
+   // Hide the dropdown
   document.getElementById("diffDropdown").classList.remove("show");
 }
-
+// Hide or show the rules
 function hideRules() {
   let rules = document.getElementById("rules-container");
   if (rules.style.display === "none") {
@@ -21,144 +42,151 @@ function hideRules() {
   }
 }
 
+
+
+
+
+
 const easyQuestions = [
-  {
-    question: "Love, Love me ___?",
-    answer: "do",
-    option1: "do",
-    option2: "to",
-    option3: "more",
-    option4: "true",
-  },
+    {
+      question: "Love, Love me ___?",
+      answers: [
+        { text: "do", correct: true },
+        { text: "to", correct: false },
+        { text: "more", correct: false },
+        { text: "true", correct: false }
+      ]
+    },
+    {
+      question: "You say goodbye, and I say ___?",
+      answers: [
+        { text: "goodbye", correct: false },
+        { text: "hello", correct: true },
+        { text: "goodnight", correct: false },
+        { text: "goodmorning", correct: false }
+      ]
+    },
+    {
+      question: "I get by with a little help from my ___?",
+      answers: [
+        { text: "friends", correct: true },
+        { text: "family", correct: false },
+        { text: "pets", correct: false },
+        { text: "neighbors", correct: false }
+      ]
+    },
+    {
+      question: "I wanna hold your ___?",
+      answers: [
+        { text: "hand", correct: true },
+        { text: "heart", correct: false },
+        { text: "head", correct: false },
+        { text: "hair", correct: false }
+      ]
+    },
+    {
+      question: "All you need is ___?",
+      answers: [
+        { text: "love", correct: true },
+        { text: "money", correct: false },
+        { text: "quiz", correct: false },
+        { text: "friends", correct: false }
+      ]
+    }
+  ];
 
-  {
-    question: "You say goodbye, and I say ___?",
-    answer: "hello",
-    option1: "goodbye",
-    option2: "hello",
-    option3: "goodnight",
-    option4: "goodmorning",
-  },
-
-  {
-    question: "I get by with a little help from my ___?",
-    answer: "friends",
-    option1: "friends",
-    option2: "family",
-    option3: "pets",
-    option4: "neighbors",
-  },
-
-  {
-    question: "I wanna hold your ___?",
-    answer: "hand",
-    option1: "hand",
-    option2: "heart",
-    option3: "head",
-    option4: "hair",
-  },
-
-  {
-    question: "All you need is ___?",
-    answer: "love",
-    option1: "love",
-    option2: "money",
-    option3: "quiz",
-    option4: "friends",
-  },
-];
-
-const mediumQuestions = [
+  const mediumQuestions = [
   {
     question: 'Which album is the song "Can\'t Buy Me Love" on?',
-    answer: "A Hard Day's Night",
-    option1: "A Hard Day's Night",
-    option2: "Help!",
-    option3: "Beatles For Sale",
-    option4: "With The Beatles",
+    answers: [
+      { text: "A Hard Day's Night", correct: true },
+      { text: "Help!", correct: false },
+      { text: "Beatles For Sale", correct: false },
+      { text: "With The Beatles", correct: false }
+    ]
   },
-
   {
     question: 'Which album is the song "I Want To Hold Your Hand" on?',
-    answer: "Meet The Beatles",
-    option1: "Meet The Beatles",
-    option2: "Please Please Me",
-    option3: "With The Beatles",
-    option4: "A Hard Day's Night",
+    answers: [
+      { text: "Meet The Beatles", correct: true },
+      { text: "Please Please Me", correct: false },
+      { text: "With The Beatles", correct: false },
+      { text: "A Hard Day's Night", correct: false }
+    ]
   },
-
   {
     question: 'Which album is the song "I am the Walrus" on?',
-    answer: "Magical Mystery Tour",
-    option1: "Magical Mystery Tour",
-    option2: "Yellow Submarine",
-    option3: "Abbey Road",
-    option4: "Let It Be",
+    answers: [
+      { text: "Magical Mystery Tour", correct: true },
+      { text: "Yellow Submarine", correct: false },
+      { text: "Abbey Road", correct: false },
+      { text: "Let It Be", correct: false }
+    ]
   },
-
   {
     question: 'Which album is the song "Let It Be" on?',
-    answer: "Let It Be",
-    option1: "Let It Be",
-    option2: "Abbey Road",
-    option3: "Magical Mystery Tour",
-    option4: "Yellow Submarine",
+    answers: [
+      { text: "Let It Be", correct: true },
+      { text: "Abbey Road", correct: false },
+      { text: "Magical Mystery Tour", correct: false },
+      { text: "Yellow Submarine", correct: false }
+    ]
   },
-
   {
     question: 'Which album is the song "I Feel Fine" on?',
-    answer: "Beatles For Sale",
-    option1: "Beatles For Sale",
-    option2: "Help!",
-    option3: "A Hard Day's Night",
-    option4: "With The Beatles",
-  },
+    answers: [
+      { text: "Beatles For Sale", correct: true },
+      { text: "Help!", correct: false },
+      { text: "A Hard Day's Night", correct: false },
+      { text: "With The Beatles", correct: false }
+    ]
+  }
 ];
 
 const hardQuestions = [
   {
     question: "Who was the Beatles original drummer?",
-    answer: "Pete Best",
-    option1: "Ringo Starr",
-    option2: "Pete Best",
-    option3: "Keith Moon",
-    option4: "Charlie Watts",
+    answers: [
+      { text: "Ringo Starr", correct: false },
+      { text: "Pete Best", correct: true },
+      { text: "Keith Moon", correct: false },
+      { text: "Charlie Watts", correct: false }
+    ]
   },
-
   {
     question: "Who was the oldest Beatle?",
-    answer: "Ringo Starr",
-    option1: "John Lennon",
-    option2: "Paul McCartney",
-    option3: "George Harrison",
-    option4: "Ringo Starr",
+    answers: [
+      { text: "John Lennon", correct: false },
+      { text: "Paul McCartney", correct: false },
+      { text: "George Harrison", correct: false },
+      { text: "Ringo Starr", correct: true }
+    ]
   },
-
   {
     question: "Which Beatle was the first to get married?",
-    answer: "John Lennon",
-    option1: "John Lennon",
-    option2: "Paul McCartney",
-    option3: "George Harrison",
-    option4: "Ringo Starr",
+    answers: [
+      { text: "John Lennon", correct: true },
+      { text: "Paul McCartney", correct: false },
+      { text: "George Harrison", correct: false },
+      { text: "Ringo Starr", correct: false }
+    ]
   },
-
   {
     question: "How many number one albums did the Beatles have in the UK?",
-    answer: "15",
-    option1: "15",
-    option2: "10",
-    option3: "20",
-    option4: "25",
+    answers: [
+      { text: "15", correct: true },
+      { text: "10", correct: false },
+      { text: "20", correct: false },
+      { text: "25", correct: false }
+    ]
   },
-
   {
-
     question: 'What is Ringo Starr\'s real name?',
-    answer: "Richard Starkey",
-    option1: "Richard Starkey",
-    option2: "Richard Starr",
-    option3: "Ringo Starr",
-    option4: "Richard Starkey Jr.",
+    answers: [
+      { text: "Richard Starkey", correct: true },
+      { text: "Richard Starr", correct: false },
+      { text: "Ringo Starr", correct: false },
+      { text: "Richard Starkey Jr.", correct: false }
+    ]
   }
 ];
