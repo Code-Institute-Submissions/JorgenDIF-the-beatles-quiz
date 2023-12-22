798f55dba498708d49071af4c2d4fe3c69495645
JorgenDIF <jorgenjonsson80@gmail.com>
2023-12-22 10:21:52 +0100

    update high score sort in end.js

--- assets/js/end.js
+++ assets/js/end.js
@@ -2,8 +2,11 @@ const username = document.getElementById("username");
 const saveScoresBtn = document.getElementById("saveScoresBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
-finalScore.innerText = mostRecentScore;
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+console.log(highScores);
+
+finalScore.innerText = mostRecentScore;
 username.addEventListener("keyup", () => {
     console.log(username.value);
     saveScoresBtn.disabled = !username.value;
@@ -12,6 +15,15 @@ username.addEventListener("keyup", () => {
 saveHighScore = (e) => {
     console.log("clicked the save button!");
     e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value,
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score) 
+    
+console.log(highScores);
 };
 
 
