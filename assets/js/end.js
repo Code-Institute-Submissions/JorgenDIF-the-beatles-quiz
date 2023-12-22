8f23db325517168d0a735831f6fff23fba6ba26f
JorgenDIF <jorgenjonsson80@gmail.com>
2023-12-22 12:22:55 +0100

    create high score html and js files

--- assets/js/end.js
+++ assets/js/end.js
@@ -4,26 +4,26 @@ const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-console.log(highScores);
+const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 username.addEventListener("keyup", () => {
-    console.log(username.value);
-    saveScoresBtn.disabled = !username.value;
+  
+  saveScoresBtn.disabled = !username.value;
 });
 
 saveHighScore = (e) => {
-    console.log("clicked the save button!");
-    e.preventDefault();
+  console.log("clicked the save button!");
+  e.preventDefault();
 
-    const score = {
-        score: mostRecentScore,
-        name: username.value,
-    };
-    highScores.push(score);
-    highScores.sort((a, b) => b.score - a.score) 
-    
-console.log(highScores);
-};
+  const score = {
+    score: Math.floor(Math.random() * 100),
+    name: username.value,
+  };
+  highScores.push(score);
+  highScores.sort((a, b) => b.score - a.score);
+highScores.splice(5);
 
-
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+  
+  };
